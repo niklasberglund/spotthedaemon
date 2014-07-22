b445adf139529195c5dd7b69addfd01d397d3bca
niklasberglund <niklas@asdasd.net>
2014-07-22 18:40:18 +0800

    added SDCommandServer

---
+++ Empty CocoaLibSpotify Project/Empty CocoaLibSpotify Project/SDCommandServer.h
@@ -0,0 +1,21 @@
+//
+//  SDCommandServer.h
+//  Empty CocoaLibSpotify Project
+//
+//  Created by Niklas Berglund on 7/2/14.
+//  Copyright (c) 2014 Your Company. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "GCDAsyncSocket.h"
+
+@interface SDCommandServer : NSObject<GCDAsyncSocketDelegate>
+{
+    GCDAsyncSocket *listenSocket;
+    NSMutableArray *activeSockets;
+    dispatch_queue_t socketQueue;
+}
+
+- (void)start;
+
+@end
---
+++ Empty CocoaLibSpotify Project/Empty CocoaLibSpotify Project/SDCommandServer.m
@@ -0,0 +1,116 @@
+//
+//  SDCommandServer.m
+//  Empty CocoaLibSpotify Project
+//
+//  Created by Niklas Berglund on 7/2/14.
+//  Copyright (c) 2014 Your Company. All rights reserved.
+//
+
+#import "SDCommandServer.h"
+
+@implementation SDCommandServer
+
+
+- (id)init
+{
+    self = [super init];
+    
+    if (self) {
+        self->activeSockets = [NSMutableArray array];
+    }
+    
+    return self;
+}
+
+
+- (void)start
+{
+    self->listenSocket = [[GCDAsyncSocket alloc] initWithDelegate:self delegateQueue:dispatch_get_main_queue()];
+    NSLog(@"%@", self->listenSocket);
+    
+    NSError *startListenError;
+    
+    //BOOL startSuccess = [self->listenSocket acceptOnInterface:@"en0" port:LISTEN_PORT error:&startListenError];
+    BOOL startSuccess = [self->listenSocket acceptOnPort:4030 error:&startListenError];
+    
+    if (!startSuccess) {
+        NSLog(@"error");
+    }
+    
+    NSLog(@"listening on port %i", LISTEN_PORT);
+    NSLog(@"%@", startListenError);
+}
+
+
+- (void)socket:(GCDAsyncSocket *)sender didAcceptNewSocket:(GCDAsyncSocket *)newSocket
+{
+    NSLog(@"accepting socket");
+    NSMutableDictionary *socketDict = [NSMutableDictionary dictionaryWithObjects:@[newSocket, [NSData data]] forKeys:@[@"socket", @"data"]];
+    [self->activeSockets addObject:socketDict];
+    NSLog(@"accepted socket %@", newSocket);
+    
+    [newSocket readDataToData:[GCDAsyncSocket CRLFData] withTimeout:60.0 tag:0];
+}
+
+
+- (void)socketDidDisconnect:(GCDAsyncSocket *)sock withError:(NSError *)error
+{
+    NSDictionary *currentSocketDict;
+    
+    for (NSDictionary *socketDict in [self->activeSockets copy]) {
+        if ([socketDict objectForKey:@"socket"] == sock) {
+            currentSocketDict = socketDict;
+            [self->activeSockets removeObject:socketDict];
+        }
+    }
+    
+    if (error) {
+        NSLog(@"ERROR: Socket disconnected with error");
+        NSLog(@"%@", error);
+    }
+}
+
+- (void)socket:(GCDAsyncSocket *)sock didConnectToHost:(NSString *)host port:(uint16_t)port
+{
+    NSLog(@"%@", host);
+}
+
+
+- (void)socket:(GCDAsyncSocket *)sock didReadPartialDataOfLength:(NSUInteger)partialLength tag:(long)tag
+{
+    NSLog(@"read partial data of length %i", partialLength);
+}
+
+
+- (void)socket:(GCDAsyncSocket *)sock didReadData:(NSData *)data withTag:(long)tag
+{
+    NSMutableData *socketData;
+    
+    NSLog(@"looking for this socket's data");
+    for (NSMutableDictionary *socketDict in [self->activeSockets copy]) {
+        if ([socketDict objectForKey:@"socket"] == sock) {
+            socketData = [socketDict objectForKey:@"data"];
+            
+            if (socketData == nil) {
+                socketData = [[NSMutableData alloc] init];
+                [socketDict setValue:socketData forKey:@"data"];
+            }
+        }
+    }
+    
+    // if it's nil at this point then something is wrong
+    if (socketData == nil) {
+        NSLog(@"ERROR: socketData is nil. cannot read data from socket");
+        return;
+    }
+    
+    NSString *dataString = [[NSString alloc] initWithData:socketData encoding:NSUTF8StringEncoding];
+    
+    NSLog(@"socket data:");
+    NSLog(@"%@", dataString);
+    
+    //[sock readDataToData:[GCDAsyncSocket CRLFData] withTimeout:60.0 tag:0];
+}
+
+
+@end

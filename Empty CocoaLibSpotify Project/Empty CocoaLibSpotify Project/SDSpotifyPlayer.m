329dd677c1c099ca9c3878cfc45c7cf11d71217d
niklasberglund <niklas@asdasd.net>
2014-08-08 00:31:25 +0800

    SPSessionDelegate methods in SDSpotifyPlayer

--- Empty CocoaLibSpotify Project/Empty CocoaLibSpotify Project/SDSpotifyPlayer.m
+++ Empty CocoaLibSpotify Project/Empty CocoaLibSpotify Project/SDSpotifyPlayer.m
@@ -10,6 +10,9 @@
 
 #include "appkey.c"
 
+#define SP_LIBSPOTIFY_DEBUG_LOGGING 0
+
+
 @implementation SDSpotifyPlayer
 
 - (id)init
@@ -46,6 +49,67 @@
     }
     
     return singletonInstance;
+}
+
+#pragma mark -
+#pragma mark SPSessionDelegate Methods
+
+-(void)sessionDidLoginSuccessfully:(SPSession *)aSession {
+	// Called after a successful login.
+}
+
+-(void)session:(SPSession *)aSession didFailToLoginWithError:(NSError *)error {
+	// Called after a failed login.
+    [NSApp presentError:error
+         modalForWindow:self.window
+               delegate:nil
+     didPresentSelector:nil
+            contextInfo:nil];
+}
+
+-(void)sessionDidLogOut:(SPSession *)aSession; {
+	// Called after a logout has been completed.
+}
+
+-(void)session:(SPSession *)aSession didGenerateLoginCredentials:(NSString *)credential forUserName:(NSString *)userName {
+    
+	// Called when login credentials are created. If you want to save user logins, uncomment the code below.
+    
+	/*
+	 NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+	 NSMutableDictionary *storedCredentials = [[defaults valueForKey:@"SpotifyUsers"] mutableCopy];
+     
+	 if (storedCredentials == nil)
+	 storedCredentials = [NSMutableDictionary dictionary];
+     
+	 [storedCredentials setValue:credential forKey:userName];
+	 [defaults setValue:storedCredentials forKey:@"SpotifyUsers"];
+	 */
+}
+
+-(void)session:(SPSession *)aSession didEncounterNetworkError:(NSError *)error; {
+	if (SP_LIBSPOTIFY_DEBUG_LOGGING != 0)
+		NSLog(@"CocoaLS NETWORK ERROR: %@", error);
+}
+
+-(void)session:(SPSession *)aSession didLogMessage:(NSString *)aMessage; {
+	if (SP_LIBSPOTIFY_DEBUG_LOGGING != 0)
+		NSLog(@"CocoaLS DEBUG: %@", aMessage);
+}
+
+-(void)sessionDidChangeMetadata:(SPSession *)aSession; {
+	// Called when metadata has been updated somewhere in the
+	// CocoaLibSpotify object model. You don't normally need to do
+	// anything here. KVO on the metadata you're interested in instead.
+}
+
+-(void)session:(SPSession *)aSession recievedMessageForUser:(NSString *)aMessage; {
+	// Called when the Spotify service wants to relay a piece of information to the user.
+	[[NSAlert alertWithMessageText:aMessage
+					 defaultButton:@"OK"
+				   alternateButton:@""
+					   otherButton:@""
+		 informativeTextWithFormat:@"This message was sent to you from the Spotify service."] runModal];
 }
 
 @end

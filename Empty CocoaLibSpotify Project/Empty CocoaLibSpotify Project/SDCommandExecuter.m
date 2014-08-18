0a8e013ea24ad10e74ce11871600fa43bad53bb1
niklasberglund <niklas@asdasd.net>
2014-08-18 22:13:21 +0800

    implemented status command in daemon

--- Empty CocoaLibSpotify Project/Empty CocoaLibSpotify Project/SDCommandExecuter.m
+++ Empty CocoaLibSpotify Project/Empty CocoaLibSpotify Project/SDCommandExecuter.m
@@ -58,12 +58,71 @@
         NSString *password = [command.arguments objectAtIndex:1];
         [[SDSpotifyPlayer sharedPlayer] loginUser:username password:password];
     }
+    else if ([command.commandString isEqualToString:@"status"]) {
+        [self executeStatusCommandFromSocket:socket];
+    }
 }
 
 
 - (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context
 {
     
+}
+
+
+/*
+ SP_CONNECTION_STATE_LOGGED_OUT
+ User not yet logged in.
+ 
+ SP_CONNECTION_STATE_OFFLINE
+ User is logged in but in offline mode.
+ 
+ SP_CONNECTION_STATE_LOGGED_IN
+ Logged in against a Spotify access point.
+ 
+ SP_CONNECTION_STATE_DISCONNECTED
+ Was logged in, but has now been disconnected.
+ 
+ SP_CONNECTION_STATE_UNDEFINED
+ */
+- (void)executeStatusCommandFromSocket:(GCDAsyncSocket *)socket
+{
+    NSMutableDictionary *responseDataDict = [[NSMutableDictionary alloc] init];
+    
+    sp_connectionstate connectionState = [[SPSession sharedSession] connectionState];
+    NSString *connectionStateString;
+    
+    switch (connectionState) {
+        case SP_CONNECTION_STATE_LOGGED_OUT:
+            connectionStateString = @"Logged out";
+            break;
+        
+        case SP_CONNECTION_STATE_LOGGED_IN:
+            connectionStateString = @"Logged in";
+            break;
+        
+        case SP_CONNECTION_STATE_OFFLINE:
+            connectionStateString = @"Offline but logged in";
+            break;
+        
+        case SP_CONNECTION_STATE_DISCONNECTED:
+            connectionStateString = @"Was logged in, but now disconnected";
+            break;
+        
+        case SP_CONNECTION_STATE_UNDEFINED:
+            connectionStateString = @"Undefined state";
+            break;
+            
+        default:
+            break;
+    }
+    
+    
+    [responseDataDict setObject:[NSNumber numberWithInt:connectionState] forKey:@"connection_state_identifier"];
+    SDResponse *response = [SDResponse responseWithMessage:connectionStateString success:nil data:responseDataDict];
+    
+    [socket writeData:[response json] withTimeout:60.0 tag:0];
+    [socket disconnectAfterWriting];
 }
 
 @end

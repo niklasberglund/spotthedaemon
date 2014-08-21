d7d5f3ebd9d62ba7b5e5a8b97425250ab4aa9048
niklasberglund <niklas@asdasd.net>
2014-08-21 13:55:40 +0800

    created SDSpotDaDaemon

---
+++ Empty CocoaLibSpotify Project/Empty CocoaLibSpotify Project/SDSpotDaDaemon.h
@@ -0,0 +1,14 @@
+//
+//  SDSpotDaDaemon.h
+//  SpotDaDaemon
+//
+//  Created by Niklas Berglund on 8/21/14.
+//  Copyright (c) 2014 Your Company. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "CocoaLumberjack.h"
+
+@interface SDSpotDaDaemon : NSObject
+
+@end
---
+++ Empty CocoaLibSpotify Project/Empty CocoaLibSpotify Project/SDSpotDaDaemon.m
@@ -0,0 +1,15 @@
+//
+//  SDSpotDaDaemon.m
+//  SpotDaDaemon
+//
+//  Created by Niklas Berglund on 8/21/14.
+//  Copyright (c) 2014 Your Company. All rights reserved.
+//
+
+#import "SDSpotDaDaemon.h"
+
+@implementation SDSpotDaDaemon
+
+static const int ddLogLevel = LOG_LEVEL_VERBOSE;
+
+@end
